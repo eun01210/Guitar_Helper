66b8ff4b5370e0482ef1a36550b4ddfbedab061b
eun01210 <eun01210@users.noreply.github.com>
2025-12-02 05:28:15 +0000

    deploy: 4eaebe64f639057df8002923b604107661bec9bc

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2305728189"
+    serviceWorkerVersion: "1206697662"
   }
 });
