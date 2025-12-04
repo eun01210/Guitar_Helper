c8b15f44a00197aa6ad0b78001e14c144e8c9f5f
unknown <hwangboeun2002@naver.com>
2025-12-04 10:55:35 +0900

    metronome safari fix

--- lib/util/audio_unlock_stub.dart
+++ lib/util/audio_unlock_stub.dart
@@ -1,3 +1,1 @@
-void unlockWebAudio() {
-  // 웹이 아닌 플랫폼에서는 아무 작업도 하지 않습니다.
-}
+void unlockWebAudio() {}
--- lib/util/audio_unlock_web.dart
+++ lib/util/audio_unlock_web.dart
@@ -1,11 +1,10 @@
 import 'dart:js_interop';
 
-// JavaScript의 eval 함수를 Dart에서 호출할 수 있도록 선언합니다.
+// safari 자동 재생 정책 해결
 @JS('eval')
 external void _eval(String code);
 
 void unlockWebAudio() {
-  // Safari의 오디오 자동 재생 정책을 해결합니다.
   _eval(
     "if (window.metCtx && window.metCtx.state === 'suspended') { window.metCtx.resume(); }",
   );
